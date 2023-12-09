f1753ae8a67511a4f2e2670ac0fa1253e4d68073
Asaf Naory <asaf.naory1@gmail.com>
2023-12-09 20:25:45 +0200

    Fix port number parsing in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import { loadEnv } from "vite";
 const { PORT } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
-const port =  Number(PORT) ||  process.env.PORT || 4321;
+const port =  Number(PORT) ||  Number(process.env.PORT) || 4321;
 console.log(`Port: ${port}`, PORT);
 export default defineConfig({
   server: { port, host: '0.0.0.0'},
