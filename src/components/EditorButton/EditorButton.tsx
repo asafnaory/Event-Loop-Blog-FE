b1a3c0e478a0ef2c0358b998e0df98054c76ec30
Asaf Naory <asaf.naory1@gmail.com>
2023-12-18 20:17:55 +0200

    add mobile view

--- src/components/EditorButton/EditorButton.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./EditorButton.module.css";
-
-export function EditorButton({text, onClick}: {text: string, onClick: () => void}){
-    return(
-        <button className={styles["editor-button"]} onClick={onClick}>{text}</button>
-    )
-}
--- src/components/types.ts
+++
@@ -1,6 +0,0 @@
-export type ImageType = {
-  src: string;
-  width: number;
-  height: number;
-  format?: "png" | "jpg" | "jpeg" | "tiff" | "webp" | "gif" | "svg" | "avif";
-}
