00bdf1fdb7482a9f6bdc2e4c34ba37c083e7089b
Asaf Naory <asaf.naory1@gmail.com>
2023-12-29 18:24:56 +0200

    Update CSS imports and add global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,8 @@ const {
 const port = Number(PORT) || Number(process.env.PORT) || 4321;
 console.log(`Port: ${port}`, PORT);
 
-// https://astro.build/config
 export default defineConfig({
-  // output: 'hy',
-  // server: {
-  //   port,
-  //   host: '0.0.0.0'
-  // },
+  output: "hybrid",
   integrations: [react(), mdx()],
   adapter: node({
     mode: "standalone"
