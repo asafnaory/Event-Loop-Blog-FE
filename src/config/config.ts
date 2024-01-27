c735672a37760c85b0a281360e54452e7347ca1b
Asaf Naory <asaf.naory1@gmail.com>
2024-01-27 09:11:16 +0200

    Update config.ts to log import.meta.env variables with descriptive labels

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
 const isProd = import.meta.env.MODE === 'production';
-console.log(import.meta.env.MODE);
-console.log(import.meta.env.BASE_URL);
-console.log(import.meta.env.SITE);
-console.log(import.meta.env.ASSETS_PREFIX);
-console.log(import.meta.env.PUBLIC_BASE_URL);
+console.log('import.meta.env.MODE', import.meta.env.MODE);
+console.log('import.meta.env.BASE_URL', import.meta.env.BASE_URL);
+console.log('import.meta.env.SITE', import.meta.env.SITE);
+console.log('import.meta.env.ASSETS_PREFIX' , import.meta.env.ASSETS_PREFIX);
+console.log('import.meta.env.PUBLIC_BASE_URL', import.meta.env.PUBLIC_BASE_URL);
 
-export const baseUrl = import.meta.env.PUBLIC_BASE_URl || 'http://localhost:4321';
+export const baseUrl = import.meta.env.PUBLIC_BASE_URL || 'http://localhost:4321';
