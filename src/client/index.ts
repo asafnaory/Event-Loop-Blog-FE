f2c20f86c03c1be5777bfdc65ed605b3435e181a
Asaf Naory <asaf.naory1@gmail.com>
2024-02-14 16:29:54 +0200

    Add console log for TRPC baseUrl

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,6 +2,7 @@ import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import { baseUrl } from '../config/config';
 import type { AppRouter } from '../server';
 
+console.log('TRPC baseUrl', baseUrl);
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
